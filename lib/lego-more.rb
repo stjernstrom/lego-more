8d549061669c6e7a2a3e0ab3818636778b604db9
Mathias Stjernström <mathias@globalinn.com>
2010-01-13 23:42:25 +0100

    Spelling error

--- lib/lego-more.rb
+++ lib/lego-more.rb
@@ -3,6 +3,6 @@ module Lego
   file_path = File.expand_path( File.dirname(__FILE__) )
   $LOAD_PATH.unshift( file_path ) unless $LOAD_PATH.include?( file_path )
 
-  autoload :BasicRouter, "more/basic_router.rb"
+  autoload :BasicRouter, "lego/basic_router.rb"
 
 end
